2000dd9c9d7153cf6c0424e971f68f9027ae8c08
Folareen <sakawahab03@gmail.com>
2023-06-14 20:56:01 +0100

    setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,45 @@
-const express = require('express')
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import authRouter from './routes/auth'
+import cloudinary from 'cloudinary'
+import connectToDB from './utils/connectToDB'
+import fileUpload from 'express-fileupload'
+
+
+dotenv.config()
+
 const app = express()
 
-app.listen(5000, ()=> {
-    console.log('server started at port 5000')
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp'
+}))
+
+app.use('/api/v1', authRouter)
+
+const PORT = process.env.PORT || 5000
+
+const startServer = async () => {
+    try {
+        await connectToDB()
+        app.listen(PORT, () => {
+            console.log(`Server started at port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+        console.log('Unable to start server')
+    }
+}
+
+startServer()
