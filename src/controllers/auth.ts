7c0acc7ee698eb465e03111511d073737834c5ea
Folareen <sakawahab03@gmail.com>
2023-06-18 22:31:21 +0100

    changed reset password mail subject

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -135,7 +135,7 @@ export const requestPasswordReset = async (req: Request, res: Response) => {
         const mailOptions = {
             from: 'sakawahab03@gmail.com',
             to: user.emailAddress,
-            subject: 'Password reset token',
+            subject: 'trakka password reset token',
             html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
